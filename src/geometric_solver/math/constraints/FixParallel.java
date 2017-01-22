3638ba413d2d4e344687793afb662ebefc366e5a
Alegria <anna151094@mail.ru>
2017-01-22 23:37:51 +0300

    add doubleDiff for FixParallel

--- src/geometric_solver/math/constraints/FixParallel.java
+++ src/geometric_solver/math/constraints/FixParallel.java
@@ -71,6 +71,213 @@ public class FixParallel extends Constraint {
 
     @Override
     public double doubleDiff(Variable varOne, Variable varTwo, Source source) {
+        // X1
+        Variable x1 = firstLine.getP1().getSquaredSummX().getVariable();
+        // X2
+        Variable x2 = firstLine.getP2().getSquaredSummX().getVariable();
+        // X3
+        Variable x3 = secondLine.getP1().getSquaredSummX().getVariable();
+        // X4
+        Variable x4 = secondLine.getP2().getSquaredSummX().getVariable();
+        // Y1
+        Variable y1 = firstLine.getP1().getSquaredSummY().getVariable();
+        // Y2
+        Variable y2 = firstLine.getP2().getSquaredSummY().getVariable();
+        // Y3
+        Variable y3 = secondLine.getP1().getSquaredSummY().getVariable();
+        // Y4
+        Variable y4 = secondLine.getP2().getSquaredSummY().getVariable();
+        if (x1.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (y4.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(y3) - source.getValue(y4);
+            }
+            return 0.0;
+        } else if (x2.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (y4.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(y4) - source.getValue(y3);
+            }
+            return 0.0;
+        } else if (x3.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (y2.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(y2) - source.getValue(y1);
+            }
+            return 0.0;
+        } else if (x4.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (y2.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(y1) - source.getValue(y2);
+            }
+            return 0.0;
+        } else if (y1.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (x4.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(x4) - source.getValue(x3);
+            }
+            return 0.0;
+        } else if (y2.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return 0.0;
+            } else if (x2.equals(varTwo)) {
+                return 0.0;
+            } else if (x3.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (x4.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(x3) - source.getValue(x4);
+            }
+            return 0.0;
+        } else if (y3.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (x2.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(x1) - source.getValue(x2);
+            }
+            return 0.0;
+        } else if (y4.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return -source.getValue(lambda);
+            } else if (x2.equals(varTwo)) {
+                return source.getValue(lambda);
+            } else if (x3.equals(varTwo)) {
+                return 0.0;
+            } else if (x4.equals(varTwo)) {
+                return 0.0;
+            } else if (y1.equals(varTwo)) {
+                return 0.0;
+            } else if (y2.equals(varTwo)) {
+                return 0.0;
+            } else if (y3.equals(varTwo)) {
+                return 0.0;
+            } else if (y4.equals(varTwo)) {
+                return 0.0;
+            } else if (lambda.equals(varTwo)) {
+                return source.getValue(x2) - source.getValue(x1);
+            }
+            return 0.0;
+        } else if (lambda.equals(varOne)) {
+            if (x1.equals(varTwo)) {
+                return source.getValue(y3) - source.getValue(y4);
+            } else if (x2.equals(varTwo)) {
+                return -source.getValue(y3) + source.getValue(y4);
+            } else if (x3.equals(varTwo)) {
+                return source.getValue(y2) - source.getValue(y1);
+            } else if (x4.equals(varTwo)) {
+                return source.getValue(y1) - source.getValue(y2);
+            } else if (y1.equals(varTwo)) {
+                return source.getValue(x4) - source.getValue(x3);
+            } else if (y2.equals(varTwo)) {
+                return source.getValue(x3) - source.getValue(x4);
+            } else if (y3.equals(varTwo)) {
+                return source.getValue(x1) - source.getValue(x2);
+            } else if (y4.equals(varTwo)) {
+                return source.getValue(x2) - source.getValue(x1);
+            } else if (lambda.equals(varTwo)) {
+                return 0.0;
+            }
+            return 0.0;
+        }
+
         return 0.0;
     }
 
